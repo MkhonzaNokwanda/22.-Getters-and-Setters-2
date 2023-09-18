0a6914062baeed1d340e5c5fbd447dfb891c62c1
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-18 14:25:04 +0200

    Add project files.

---
+++ 22. Getters and Setters 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Getters_and_Setters_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student("Nono",18,"Female");
+            Student stu2 = new Student("Penguin",17,"Male");
+            Student stu3 = new Student("Mimi",39,"hggf");
+
+            Console.WriteLine(Student.studentCount);
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 22. Getters and Setters 2/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Getters_and_Setters_2
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+        public static int studentCount=0;
+
+        //Constructors
+        public Student(string aName, int aAge,string aGender)
+        {
+            name = aName;
+            age = aAge;
+            gender = aGender;
+            studentCount++;
+        }
+        public string Gender
+        { 
+            get
+            { return gender; } 
+            set 
+            {
+                    if (value == "Female" || value == "Male")
+                    { gender = value; }
+                    else
+                    { gender = "Non-binary"; }
+            }
+        }
+    }
+}
